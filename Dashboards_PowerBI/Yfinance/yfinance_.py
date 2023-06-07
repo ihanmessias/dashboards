8fc02fd67f4e94864a77f468a04f49505a577a6c
mrhowaito <ihanmessias.dev@gmail.com>
2023-06-06 21:44:33 -0300

    update analysis

--- Dashboards_PowerBI/Yfinance/yfinance_.py
+++ Dashboards_PowerBI/Yfinance/yfinance_.py
@@ -6,6 +6,14 @@ def loading_data(ticker):
     df['ticker'] = ticker.split('.')[0]
     return df
 
-petrobras = loading_data('PETR4.SA')
-bb = loading_data('BBAS3.SA')
+petroleo_brasileiro_petrobras = loading_data('PETR4.SA')
+banco_do_brasil_bb = loading_data('BBAS3.SA')
 vale = loading_data('VALE3.SA')
+tesla_inc = loading_data('TSLA.NE')
+apple_inc = loading_data('AAPL.NE')
+amazon = loading_data('AMZN.NE')
+intel = loading_data('INTC.NE')
+netfilx = loading_data('NFLX.NE')
+alphabet_inc_google = loading_data('GOOG.NE')
+nvidia_corporation = loading_data('NVDA.NE')
+advanced_micro_devices_inc = loading_data('AMD.NE')
